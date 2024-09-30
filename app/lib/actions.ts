bc349f3a211d734ba43b5edf3664b2ff9e68f22b
Veli Can Aydın <30829560+VeliCanAydin@users.noreply.github.com>
2024-09-30 21:50:35 +0300

    chapter 13 fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,20 +21,21 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
   try {
     await sql`
-    console.error(error);
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
     return {
-      message: 'Database Error: Failed to create invoice',
+      message: 'Database Error: Failed to Create Invoice.',
     };
   }
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -52,14 +53,12 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   try {
     await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
   } catch (error) {
-    return {
-      message: 'Database Error: Failed to update invoice',
-    };
+    return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
   revalidatePath('/dashboard/invoices');
@@ -67,15 +66,11 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted invoice' };
+    return { message: 'Deleted Invoice.' };
   } catch (error) {
-    return {
-      message: 'Database Error: Failed to delete invoice',
-    };
+    return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
